478ebf93dfe5f06bd1ef619c8824715cfb384c0d
NatGames <salavat-iskhakov@mail.ru>
2022-06-03 15:41:22 +0300

    create hooks & ComicsList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,7 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import ComicsList from "../comicsList/ComicsList";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -19,17 +20,18 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        <ErrorBoundary>
+        {/* <ErrorBoundary>
           <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
+        </ErrorBoundary> */}
+        {/* <div className="char__content">
           <ErrorBoundary>
             <CharList onCharSelected={onCharSelected} />
           </ErrorBoundary>
           <ErrorBoundary>
             <CharInfo charId={selectedChar} />
           </ErrorBoundary>
-        </div>
+        </div> */}
+        <ComicsList />
         <img className="bg-decoration" src={decoration} alt="vision" />
       </main>
     </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,31 +1,23 @@
+import { useHttp } from '../hooks/http.hook';
 
+const useMarvelService = () => {
+  const { loading, request, error, clearError } = useHttp();
 
-class MarvelService {
-  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiKey = 'apikey=1cbe2bf98e68c6fed4c49e2cc5ec10c0';
-  _baseOffset = 210;
+  const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  const _apiKey = 'apikey=1cbe2bf98e68c6fed4c49e2cc5ec10c0';
+  const _baseOffset = 210;
 
-  getResource = async (url) => {
-    let res = await fetch(url);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    }
-
-    return await res.json();
+  const getAllCharacters = async (offset = _baseOffset) => {
+    const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+    return res.data.results.map(_transformCharacter)
   }
 
-  getAllCharacters = async (offset = this._baseOffset) => {
-    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-    return res.data.results.map(this._transformCharacter)
+  const getCharacter = async (id) => {
+    const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+    return _transformCharacter(res.data.results[0]);
   }
-
-  getCharacter = async (id) => {
-    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-    return this._transformCharacter(res.data.results[0]);
-  }
-
-  _transformCharacter = (result) => {
+  
+  const _transformCharacter = (result) => {
     return {
       id: result.id,
       name: result.name,
@@ -40,6 +32,31 @@ class MarvelService {
       comics: result.comics.items,
     }
   }
+
+  const getAllComics = async (offset = _baseOffset) => {
+    const res = await request(`${_apiBase}comics?limit=8&offset=${offset}&${_apiKey}`);
+    console.log(res)
+    return res.data.results.map(_transformComics);
+  }
+
+  const getComics = async (id) => {
+    const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+    return _transformComics(res.data.results[0]);
+  }
+
+  const _transformComics = (result) => {
+    return {
+      id: result.id,
+      title: result.title,
+      thumbnail: result.thumbnail.path + '.' + result.thumbnail.extension,
+      price: result.prices[0].price ?
+        `${result.prices[0].price}$`
+        : 'NOT AVAILABLE',
+      homepage: result.urls[0].url,
+    }
+  }
+
+  return { loading, error, getAllCharacters, getAllComics, getCharacter, clearError };
 }
 
-export default MarvelService;
+export default useMarvelService;
